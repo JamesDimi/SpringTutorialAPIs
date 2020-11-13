b3568068bbee66504b67729a20e4c1ff8da5aaf6
ddimitrakopoulos <ddimitrakopoulos@groundzerolabs.com>
2020-11-13 16:44:58 +0200

    initializing project

---
+++ src/main/java/com/ddimi/SpringTutorialAPIs/SpringTutorialApIsApplication.java
@@ -0,0 +1,13 @@
+package com.ddimi.SpringTutorialAPIs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutorialApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTutorialApIsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddimi/SpringTutorialAPIs/SpringTutorialApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddimi.SpringTutorialAPIs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutorialApIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
